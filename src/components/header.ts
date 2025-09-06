0f63374b967678bb92b6c04f2bbaa8b4acb2c0ce
andersseen <andriipap01@gmail.com>
2025-09-06 19:16:04 +0200

    Add theme switcher

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,4 +1,5 @@
 import { Component, input, Input, output } from "@angular/core";
+import ThemeSwitcher from "./theme-switcher";
 
 @Component({
   selector: "app-header",
@@ -25,33 +26,13 @@ import { Component, input, Input, output } from "@angular/core";
               isDarkMode() ? 'Switch to light mode' : 'Switch to dark mode'
             "
           >
-            <svg
-              class="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              @if (isDarkMode()) {
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              ></path>
-              } @else {
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              ></path>
-              }
-            </svg>
+            <theme-switcher [isDarkMode]="isDarkMode()" />
           </button>
         </div>
       </div>
     </header>
   `,
+  imports: [ThemeSwitcher],
 })
 export default class Header {
   isDarkMode = input();
